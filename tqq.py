fb31f54058860cf0fe940a1d3a15be247e1ae74d
Vartika <45865677+VartikaPandey1303@users.noreply.github.com>
2019-03-30 10:40:44 +0530

    Add files via upload

---
+++ tqq.py
@@ -0,0 +1,25 @@
+class Question:
+	def __init__(self,q,a1,a2,a3,a4):
+		self.q = q
+		self.a1 = a1
+		self.a2 = a2
+		self.a3 = a3
+		self.a4 = a4
+
+
+
+q1 = Question("Admiral Gorshkov, the Soviet aircraft carrier imported by India is now named-", "INS Vikrant" , "INS Vikramaditya", "INS Virat", "INS Vishaal")
+q2 = Question("Which of the following organizations has SarvatraSarvottam Suraksha as its slogan?", "Border Security Forece", "Sikh Light Infantry", "Indian Air Force", "National Security Guard")
+q3 = Question("At which of the following places was a new Officers' Training Academy for training Army officers established in 2011?", "Pune", "Jabalpur", "Secunderabad", "Gaya")
+q4 = Question("Who of the following is the only Chief of Air Staff to be given the rank of Marshall of the Indian Air Force?", "Arjan Singh", "Subroto Mukherjee", "P.C. Lal", "O.P. Mehra")
+q5 = Question("Which of the following corps deals with the supply of weaponry and ammunition to the Indian Army?", "Army Ordnance Corps", "Armoured Corps", "Army Supply Corps", "Corps of EME")
+q6 = Question("Which of the following is home base of the aircraft carrier INS Vikramaditya?", "Visakhapatnam", "Cochin", "Karwar", "Jamnagar")
+q7 = Question("At which of the following places is the Headquarters of South Western Air Command located?", "Vadodara", "Jodhpur", "Pune", "Gandhinagar")
+q8 = Question("Which of the following is the equivalent rank in Army of the Naval rank of Captain?", "Colonel", "Brigadier", "Major", "Captain")
+q9 = Question("Who of the following was the Chief of Army at the time of Indo-Pak war of 1971?", "General PP Kumaramangalam", "Field Marshall SHFJ Manekshaw", "General JN Chaudhari","General KS Thimayya")
+q10 = Question(". Who of the following was the first recipient of the Param Vir Chakra, gallantry award?", "Lance Naik Albert Ekka", "Havildar Abdul Hamid", "Major Shaitan Singh", "Major Somnath Sharma")
+q11 = Question("Lt Commander Abhilash Tomy of the Indian Navy became the first Indian to complete a solo, unassisted, non-stop circumnavigation of the world in 2013. What was the name of the vessel used by him?", "INSV Mandovi", "INSV Mhadei", "INS Abhimanyu", "INS Abhay")
+q12 = Question(". At which of the following places is the College of Defence Management located?", "Dehradun", "Pune", "Secunderabad", "Chennai")
+q13 = Question("What was the name given to the operation conducted by National Security Guard in 2008 against terrorist attack in Taj hotel, Mumbai?", "Operation Black Tornado", "Operation Black Thunder", "Operation Safed Sagar", "Operation Vijay")
+
+rlist = [2, 4, 4, 1, 1, 3, 4, 1, 2, 4, 2, 3, 1]
